99da476504bb3567d30c2e1a1bce55a2b4e34a0f
Maksim <maksim20051708@gmail.com>
2025-04-15 01:26:55 +0300

    delete: remove docker compose

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const configService = app.get(ConfigService);
 
-  const port = process.env.PORT || 8080;
+  const port = configService.get('APP_PORT') || 8080;
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
 
   await app.listen(port, () => {
-    console.log(`Application is running on port ${port}`);
+    console.log('App is running on %s port', port);
   });
 }
 bootstrap();
